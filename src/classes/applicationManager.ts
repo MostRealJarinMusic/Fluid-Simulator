1e778244e95d6e121d165a998163abf1d10b5dc5
MostRealJarinMusic <joshjarin2006@gmail.com>
2024-02-23 00:04:07 +0000

    Application manager + rearranged listeners

---
+++ src/classes/applicationManager.ts
@@ -0,0 +1,99 @@
+class ApplicationManager {
+    //#region Private Variables
+    private airfoilGraphCanvas!: HTMLCanvasElement;
+    private ADTypeSelector!: HTMLSelectElement;
+    private ADParameterInputContainer!: HTMLDivElement;
+    private ADProfileNumText!: HTMLParagraphElement;
+    public airfoilDesigner!: AirfoilDesigner;
+
+    private FMCanvas!: HTMLCanvasElement;
+    private FMSimulationModeSelector!: HTMLSelectElement;
+    public FMTracersCheck!: HTMLInputElement;
+    public FMStreamlinesCheck!: HTMLInputElement;
+    private FMAngleOfAttackInput!: HTMLInputElement;
+    private FMFSVelocityInput!: HTMLInputElement;
+    public fluidManager!: FluidManager;
+
+    private RMCanvas!: HTMLCanvasElement;
+    private RMValuesDisplay!: HTMLDivElement;
+    private RMGraphingModeSelector!: HTMLSelectElement;
+    public resultsManager!: ResultsManager;
+    //#endregion
+
+    constructor() {
+        //Setup
+        this.setupAirfoilDesigner();
+        this.setupSimulation();
+        this.setupResultsManager();
+        this.setupListeners();
+    }
+
+    //#region Setup
+    setupAirfoilDesigner(): void {
+        //Airfoil graph
+        this.airfoilGraphCanvas = document.getElementById('airfoilGraph') as HTMLCanvasElement;
+
+        //Designer
+        this.ADTypeSelector = document.getElementById('airfoilTypeSelector') as HTMLSelectElement;
+        this.ADParameterInputContainer = document.getElementById("airfoilDesignerParameters") as HTMLDivElement;
+        this.ADProfileNumText = document.getElementById("airfoilProfileNum") as HTMLParagraphElement;
+
+        this.airfoilDesigner = new AirfoilDesigner(this.airfoilGraphCanvas, this.ADTypeSelector, this.ADParameterInputContainer, this.ADProfileNumText);
+    }
+
+    setupSimulation(): void {
+        this.FMCanvas = document.getElementById("fluidSimulation") as HTMLCanvasElement;
+        this.FMSimulationModeSelector = document.getElementById("simulationModeSelector") as HTMLSelectElement;
+        this.FMTracersCheck = document.getElementById("fluidTracers") as HTMLInputElement;
+        this.FMStreamlinesCheck = document.getElementById("fluidStreamlines") as HTMLInputElement;
+        this.FMAngleOfAttackInput = document.getElementById("angleOfAttack") as HTMLInputElement;
+        this.FMFSVelocityInput = document.getElementById("freeStreamVelocity") as HTMLInputElement;
+
+        this.fluidManager = new FluidManager(this.FMCanvas, this.FMSimulationModeSelector, this.FMAngleOfAttackInput, this.FMFSVelocityInput);
+        this.fluidManager.updateAirfoil(this.airfoilDesigner.ShapeGridPoints);
+        this.fluidManager.initFluid();
+    }
+
+    setupResultsManager(): void {
+        this.RMCanvas = document.getElementById("dataGraph") as HTMLCanvasElement;
+        this.RMValuesDisplay = document.getElementById("valuesDisplay") as HTMLDivElement;
+        this.RMGraphingModeSelector = document.getElementById("resultsGraphModeSelector") as HTMLSelectElement;
+
+        this.resultsManager = new ResultsManager(this.RMCanvas, this.RMGraphingModeSelector, this.RMValuesDisplay);
+    }
+
+    setupListeners(): void {
+        this.FMTracersCheck.addEventListener('change', function () {
+            applicationManager.fluidManager.ShowTracers = this.checked;
+        });
+        this.FMStreamlinesCheck.addEventListener('change', function () {
+            applicationManager.fluidManager.ShowStreamlines = this.checked;
+        });
+        this.ADTypeSelector.addEventListener('change', function () {
+            applicationManager.airfoilDesigner.changeAirfoil();
+            applicationManager.fluidManager.resetParameters();
+            applicationManager.fluidManager.updateAirfoil(applicationManager.airfoilDesigner.ShapeGridPoints);
+        });
+        this.FMSimulationModeSelector.addEventListener('change', function () {
+            applicationManager.fluidManager.updateSimulationMode();
+        });
+        this.FMAngleOfAttackInput.addEventListener('input', function () {
+            applicationManager.fluidManager.updateAngleOfAttack();
+        });
+        this.FMFSVelocityInput.addEventListener('change', function () {
+            applicationManager.fluidManager.updateFreeStreamVelocity();
+        });
+        this.RMGraphingModeSelector.addEventListener('change', function () {
+            applicationManager.resultsManager.updateGraphingMode();
+        });
+
+    }
+    //#endregion
+
+    update(): void {
+        for (let _ = 0; _ < stepsPerFrame; _++) {
+            this.fluidManager.runMainLoop();
+        }
+        this.fluidManager.drawFluid();
+    }
+}
--- src/renderer/renderer.ts
+++ src/renderer/renderer.ts
@@ -1,125 +1,40 @@
 //Front end
-//#region Defined SI constants for the simulation
+//#region Constants for the simulation
 const nodesPerMeter: number = 60;
 const nodeDistance: number = 1 / nodesPerMeter; //Defined 60 nodes as 1m
 const stepsPerFrame: number = 10;
 const latticeSpeedOfSound: number = 1 / Math.sqrt(3);
 //#endregion
 
-
-//#region Airfoil designer
-
-//Airfoil graph
-let airfoilGraphCanvas = document.getElementById('airfoilGraph') as HTMLCanvasElement;
-
-//Designer
-let ADTypeSelector = document.getElementById('airfoilTypeSelector') as HTMLSelectElement;
-let ADParameterInputContainer = document.getElementById("airfoilDesignerParameters") as HTMLDivElement;
-let ADProfileNumText = document.getElementById("airfoilProfileNum") as HTMLParagraphElement;
-
-const airfoilDesigner = new AirfoilDesigner(airfoilGraphCanvas, ADTypeSelector, ADParameterInputContainer, ADProfileNumText);
-//#endregion
-
-//#region Simulation
-//Simulation
-let FMCanvas = document.getElementById("fluidSimulation") as HTMLCanvasElement;
-let FMSimulationModeSelector = document.getElementById("simulationModeSelector") as HTMLSelectElement;
-let FMTracersCheck = document.getElementById("fluidTracers") as HTMLInputElement;
-let FMStreamlinesCheck = document.getElementById("fluidStreamlines") as HTMLInputElement;
-let FMFreeStreamVelocityInput = document.getElementById("freeStreamVelocity") as HTMLInputElement;
-let FMAngleOfAttackInput = document.getElementById("angleOfAttack") as HTMLInputElement;
-let FMFSVelocityInput = document.getElementById("freeStreamVelocity") as HTMLInputElement;
-
-//160, 80
-//const fluidSimulation = new Fluid(160, 80, 1, 2.5, 0.53, FMCanvas);
-const fluidManager = new FluidManager(FMCanvas, FMSimulationModeSelector, FMAngleOfAttackInput, FMFSVelocityInput);
-//fluidSimulation.setupDefaultObstacle();
-fluidManager.updateAirfoil(airfoilDesigner.ShapeGridPoints);
-fluidManager.initFluid();
-
-
-//#endregion
-
-//#region Data and Graphs
-let RMCanvas = document.getElementById("dataGraph") as HTMLCanvasElement;
-let RMValuesDisplay = document.getElementById("valuesDisplay") as HTMLDivElement;
-let RMGraphingModeSelector = document.getElementById("resultsGraphModeSelector") as HTMLSelectElement;
-
-const resultsManager = new ResultsManager(RMCanvas, RMGraphingModeSelector, RMValuesDisplay);
-
-//#endregion
-
+const applicationManager = new ApplicationManager();
 
 //#region Listener events
 function updateAirfoilParameters(): void {
-    airfoilDesigner.updateAirfoil();
-    fluidManager.updateAirfoil(airfoilDesigner.ShapeGridPoints);
+    applicationManager.airfoilDesigner.updateAirfoil();
+    applicationManager.fluidManager.updateAirfoil(applicationManager.airfoilDesigner.ShapeGridPoints);
 }
-
-function updateChosenShape(): void {
-    airfoilDesigner.changeAirfoil();
-    fluidManager.resetParameters();
-    fluidManager.updateAirfoil(airfoilDesigner.ShapeGridPoints);
-}
-
 function resetAirfoilParameters(): void {
-    airfoilDesigner.resetAirfoil();
-    fluidManager.resetParameters();
-    fluidManager.updateAirfoil(airfoilDesigner.ShapeGridPoints);
+    applicationManager.airfoilDesigner.resetAirfoil();
+    applicationManager.fluidManager.resetParameters();
+    applicationManager.fluidManager.updateAirfoil(applicationManager.airfoilDesigner.ShapeGridPoints);
 }
-
 function resetFluidParameters(): void {
-    fluidManager.resetParameters();
-    fluidManager.updateAirfoil(airfoilDesigner.ShapeGridPoints);
-
+    applicationManager.fluidManager.resetParameters();
+    applicationManager.fluidManager.updateAirfoil(applicationManager.airfoilDesigner.ShapeGridPoints);
     resetGeneralFluidControls();
 }
-
 function resetGeneralFluidControls(): void {
-    fluidManager.ShowTracers = false;
-    fluidManager.ShowStreamlines = false;
+    applicationManager.fluidManager.ShowTracers = false;
+    applicationManager.fluidManager.ShowStreamlines = false;
 
-    FMTracersCheck.checked = false;
-    FMStreamlinesCheck.checked = false;
-}
-
-
-function updateSimulationMode(): void {
-    fluidManager.updateSimulationMode();
-}
-
-function updateResultsGraphingMode(): void {
-    resultsManager.updateGraphingMode();
-}
-
-FMTracersCheck.addEventListener('change', function () {
-    fluidManager.ShowTracers = this.checked;
-});
-
-FMStreamlinesCheck.addEventListener('change', function () {
-    fluidManager.ShowStreamlines = this.checked;
-});
-
-function updateFreeStreamVelocity(): void {
-    fluidManager.updateFreeStreamVelocity();
-}
-
-/*
-FMAngleOfAttackInput.addEventListener('change', function () {
-    fluidManager.AngleOfAttack = parseFloat(this.value);
-})
-*/
-function updateAngleOfAttack(): void {
-    fluidManager.updateAngleOfAttack();
+    applicationManager.FMTracersCheck.checked = false;
+    applicationManager.FMStreamlinesCheck.checked = false;
 }
 //#endregion
 
 //#region Updating
 function update(): void {
-    for (let _ = 0; _ < stepsPerFrame; _++) {
-        fluidManager.runMainLoop();
-    }
-    fluidManager.drawFluid();
+    applicationManager.update();
     requestAnimationFrame(update);
 }
 
